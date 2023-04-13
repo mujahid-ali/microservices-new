9863d9915e49fb4038d06906ff12b4b7da8f7fe5
mujahid-ali <mujahidali0611@gmail.com>
2023-04-13 12:48:17 +0530

    Circuit breaker: resilience4j Timeout Retry Actuator Events

--- order-service/src/main/java/com/example/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/example/orderservice/service/OrderService.java
@@ -24,7 +24,7 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final WebClient.Builder webClientBuilder; //webClient variable name should be same as in WebClientConfig
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
 
@@ -51,6 +51,7 @@ public class OrderService {
 
         if (allProductsInStock) {
             orderRepository.save(order);
+            return "Order placed Successfully!!";
         } else {
             throw new IllegalArgumentException("Product is not in stock, please try again later");
         }
